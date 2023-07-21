c2825e1ab2529bdbb3180526bbc8f968ed6b17f8
서대원 <96100799+tjeodnjsdlwl@users.noreply.github.com>
2023-07-21 21:07:59 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const mainSlide = new Swiper('.sc-visual .swiper',{
+    pagination:{
+        el:".pagination"
+    },
+    navigation:{
+        nextEl:'.btn-next',
+        prevEl:".btn-prev"
+    }
+})
